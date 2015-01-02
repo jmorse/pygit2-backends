8bef619bc232b2e0b41823411dcad1a497d55d6d
Jeremy Morse <jmorse+git@studentrobotics.org>
2015-01-02 22:27:20 +0000

    Avoid hiphen in module name
    I live and learn, etc.

--- setup.py
+++ setup.py
@@ -5,15 +5,15 @@ with codecs.open('README', 'r', 'utf-8') as readme:
         long_description = readme.read()
 
 
-setup(name="pygit2-backends",
+setup(name="pygit2_backends",
         version="0.0.0",
         author="Jeremy Morse",
         author_email="jmorse+pygit2backends@studentrobotics.org",
         description="Custom backends for use with pylibgit2",
         long_description=long_description,
         license="GPL2 with linking exception",
-        packages=['pygit2-backends'],
-        ext_modules=[Extension('_pygit2-backends',
+        packages=['pygit2_backends'],
+        ext_modules=[Extension('_pygit2_backends',
             ['backends/mysql/mysql.c'],
             include_dirs=['/usr/include/mysql'],
             libraries=['git2', 'mysqlclient'])],
