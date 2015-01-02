47684073edbc19f2ada613d8105556bb83cdaa1f
Jeremy Morse <jmorse+git@studentrobotics.org>
2015-01-02 14:01:44 +0000

    Add a readme and distribute as long_descr

--- setup.py
+++ setup.py
@@ -1,8 +1,14 @@
 from distutils.core import setup
+import codecs
+
+with codecs.open('README', 'r', 'utf-8') as readme:
+        long_description = readme.read()
+
 
 setup(name="pygit2-backends",
         version="0.0.0",
         author="Jeremy Morse",
         author_email="jmorse+pygit2backends@studentrobotics.org",
         description="Custom backends for use with pylibgit2",
+        long_description=long_description,
         license="GPL2 with linking exception")
