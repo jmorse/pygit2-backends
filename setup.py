2cc997afc78391458cf5a2d46e61980251f80ea1
Jeremy Morse <jmorse+git@studentrobotics.org>
2015-01-02 13:28:13 +0000

    Add boilerplate setup.py

---
+++ setup.py
@@ -0,0 +1,8 @@
+from distutils.core import setup
+
+setup(name="pygit2-backends",
+        version="0.0.0",
+        author="Jeremy Morse",
+        author_email="jmorse+pygit2backends@studentrobotics.org",
+        description="Custom backends for use with pylibgit2",
+        license="GPL2 with linking exception")
