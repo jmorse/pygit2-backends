726e126b5e3ca32df794aa1a6b2612f0e085b8b4
Jeremy Morse <jmorse+git@studentrobotics.org>
2015-01-03 17:15:58 +0000

    Whitelist repo attrs that work in custom backends

--- pygit2_backends/repository.py
+++ pygit2_backends/repository.py
@@ -3,6 +3,8 @@ from pygit2 import Repository as PyGit2Repo
 import _pygit2_backends
 from _pygit2_backends import open_mysql_backend, create_mysql_backend
 
+supported_repo_attrs = ['TreeBuilder', 'config', 'create_blob', 'create_blob_fromdisk', 'create_blob_fromworkdir', 'create_branch', 'create_commit', 'create_reference', 'create_reference_direct', 'create_reference_symbolic', 'create_tag', 'get', 'git_object_lookup_prefix', 'is_empty', 'listall_branches', 'listall_references', 'lookup_branch', 'lookup_reference', 'merge_base', 'read', 'revparse_single', 'walk', 'write']
+
 class MysqlRepository(PyGit2Repo):
     # XXX XXX XXX dev signature, think some actual thoughts before publishing
     # this API
@@ -13,3 +15,21 @@ class MysqlRepository(PyGit2Repo):
 
         # Initialize parent class with given git repo. XXX, exceptions
         super(PyGit2Repo, self).__init__(None, repository_ptr=repo)
+
+    def __getattribute__(self, attr):
+        # Remove a ton of Repository object attributes that are out of scope
+        # when operating on a custom backend (i.e. the index, working copy,
+        # etc). It should be immediately apparent to the developer that these
+        # are not supported.
+
+        # First, potentially return an attribute error,
+        foo = super(PyGit2Repo, self).__getattribute__(attr)
+
+        # Now filter for things we support
+        if attr.startswith('_'):
+            return foo
+        if attr in supported_repo_attrs:
+            return foo
+
+        raise Exception("Attribute \"{0}\" not supported by custom git backends"
+                .format(attr))
