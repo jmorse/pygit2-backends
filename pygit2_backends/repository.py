b2f5139c124d67b648ea02858ce7ce128a77081a
Jeremy Morse <jmorse+git@studentrobotics.org>
2015-01-03 16:40:50 +0000

    Attempt to build pygit2 repo as a MysqlRepository
    This is essentially a recce to see whether everything fits together at
    this stage. Some serious consideration of error paths needs to occur
    eventually.

--- pygit2_backends/repository.py
+++ pygit2_backends/repository.py
@@ -1,6 +1,15 @@
+from __future__ import print_function
 from pygit2 import Repository as PyGit2Repo
+import _pygit2_backends
+from _pygit2_backends import open_mysql_backend, create_mysql_backend
 
 class MysqlRepository(PyGit2Repo):
-    def __init__(self, *args, **kwargs):
-        # Insert here, mechanism to mangle custom backends into existence
-        super(PyGit2Repo, self).__init__(*args, **kwargs)
+    # XXX XXX XXX dev signature, think some actual thoughts before publishing
+    # this API
+    def __init__(self, hostname, username, password, dbname, portno, unix_path):
+        # Create a struct git_repository hooked up to a mysql backend
+        repo = _pygit2_backends.open_mysql_backend(hostname, username,
+                                            password, dbname, portno, unix_path)
+
+        # Initialize parent class with given git repo. XXX, exceptions
+        super(PyGit2Repo, self).__init__(None, repository_ptr=repo)
