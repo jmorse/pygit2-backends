b6d3cf5fee96c5304cf0b2436a04745a8c2eb036
Jeremy Morse <jmorse+git@studentrobotics.org>
2015-01-10 22:35:35 +0000

    Switch license to GPL2 (drama)
    As alluded to in the comment, it's unclear what the license of these
    backends _should_ be. libmysqlclient is GPL2, but the connector code for
    libgit2 is written to be GPL2 with linking exception -- which might be
    invalid (who can say).
    
    Given that pygit2 is GPL2, and we link (broadly) with pygit2, uprate the
    license to the strongest compatible one, GPL2.

--- setup.py
+++ setup.py
@@ -11,7 +11,11 @@ setup(name="pygit2_backends",
         author_email="jmorse+pygit2backends@studentrobotics.org",
         description="Custom backends for use with pylibgit2",
         long_description=long_description,
-        license="GPL2 with linking exception",
+        license="GPL2",
+        # This is extremely unclear. The backends in the backends repo claim
+        # to be GPLv2 with linking exception, however libmysqlclient at least
+        # is definitely GPL2. Go for the strongest compatible license to
+        # reflect this fact.
         packages=['pygit2_backends'],
         ext_modules=[Extension('_pygit2_backends',
             ['src/pygit2_backends.c', 'backends/mysql/mysql.c'],
